5e3e662e698d92d48bd934f7ac92f99bcc3b7436
MaxBrants <131341205+MaxBrants@users.noreply.github.com>
2023-09-04 15:50:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'beautifulsoup4',
         'mkdocs>=1.0.4',
-        'selenium',
+        'selenium'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
